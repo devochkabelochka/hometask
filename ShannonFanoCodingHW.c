3cda80d68a6b10569c1f5157ba73c01a25d27b2b
Devochka <95773052+devochkabelochka@users.noreply.github.com>
2022-10-13 13:32:06 +0300

    added sorting, main fano function needs upgrades

--- ShannonFanoCodingHW.c
+++ ShannonFanoCodingHW.c
@@ -1,4 +1,4 @@
-#include "stdafx.h" //<stdio.h>
+#include "stdafx.h"
 
 int array[20];
 
@@ -11,44 +11,63 @@ struct my
 	char code[20];
 };
 
-
-void fano (struct my*P , int first, int last )
+void AddChar(char *x, char y)
 {
-	// когда выйти
-	// где середина по частоте nser
-	
 	int i = 0;
-	float nser;
-	while (nser < 0.5)
-	{
-		nser += p[i].chast;
-		i++;
-	}
-	
-	for (int i = first; i < nser; i++)
-	{
-		//add '0'
-	}
-	for (int i = nser; i <= last; i++)
-	{
-		//add '1'
-	}
-	fano(P, first, nser - 1);
-	fano(P, nser, last);
+	while (x[i] != '\0') i++;
+	x[i++] = y;
+	x[i] = '\0';
 }
 
-int bubble(int arr[20], int )
+void fano(my*P , int first, int last)
 {
-	return 0;
+	if (first == last) return;
+	if (first + 1 == last)
+	{
+		AddChar(P[first].code, '0');
+		AddChar(P[last].code, '1');
+		return;
+	}
+
+	int numsum, n = 0;
+	float sumchast = 0;
+
+	for (int i = first; i <= last; i++)
+	{
+		n += P[i].kol;
+	}
+	for (int i = first; i <= last; i++)
+	{
+		P[i].chast = P[i].kol / (n * 1.);
+	}
+	for (int i = first; i <= last; i++)
+	{
+		sumchast += P[i].chast;
+		if (sumchast > 0.5)
+		{
+			numsum = i;
+			break;
+		}
+	}
+
+	
+	for (int i = first; i < numsum; i++)
+	{
+		AddChar(P[i].code, '0');
+	}
+	for (int i = numsum; i <= last; i++)
+	{
+		AddChar(P[i].code, '1');
+	}
+
+	/*fano(P, first, numsum - 1);
+	fano(P, numsum, last);*/
+
 }
-
-
-
 
 int vs[256] = { 0 };
 int N;
-char str[20] = "baraban";
-
+char str[20] = "brbn";
 
 int main()
 {
@@ -69,22 +88,61 @@ int main()
 	my * new_struct = new my[N];
 
 	int j = 0;
-
 	float i_new = i;
 
 	for (int i = 0; i < 256; i++)
-
+	{
 		if (vs[i] != 0)
 		{
 			new_struct[j].ch = i;
-			new_struct[j].kol = vs[i]; 
-			new_struct[j].chast = vs[i] / i_new;
+			new_struct[j].kol = vs[i];
+		 
 			j++;
 		}
+	}
 
 	for (int i = 0; i < N; i++)
 	{
-		printf("%c  %d  %f\n", new_struct[i].ch, new_struct[i].kol, new_struct[i].chast);
+		(new_struct + i)->code[0] = '\0';
+	}
+	for (int i = 0; i < N - 1; i++)
+	{
+		int flag = 1;
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			if (new_struct[j].kol < new_struct[j + 1].kol)
+			{
+				my tmp = new_struct[j];
+				new_struct[j] = new_struct[j + 1];
+				new_struct[j + 1];
+			/*	int tmp = new_struct[j].kol;
+				char tmp_ch = new_struct[j].ch;
+				float tmp_chast = new_struct[j].chast;
+				new_struct[j].kol = new_struct[j + 1].kol;
+				new_struct[j].ch = new_struct[j + 1].ch;
+				new_struct[j].chast = new_struct[j + 1].chast;
+				new_struct[j + 1].kol = tmp;
+				new_struct[j + 1].ch = tmp_ch;
+				new_struct[j + 1].chast = tmp_chast;
+				flag = 0;*/
+			}
+		}
+		if (flag)
+		{
+			break;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%c %d %s\n", (new_struct + i)->ch, (new_struct + i)->kol, (new_struct + i)->code);
+	}
+
+	fano(new_struct, 0, N - 1);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%c  %s\n", (new_struct + i) -> ch, (new_struct + i) -> code);
 	}
 
 	return 0;
