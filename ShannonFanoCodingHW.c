fbc5efc0e04920759886ce92e05ea5e2d14a3748
Devochka <95773052+devochkabelochka@users.noreply.github.com>
2022-10-18 15:20:13 +0300

    fixed sorting and fano function
    Final version of Shennon-Fano coding on c

--- ShannonFanoCodingHW.c
+++ ShannonFanoCodingHW.c
@@ -19,7 +19,7 @@ void AddChar(char *x, char y)
 	x[i] = '\0';
 }
 
-void fano(my*P , int first, int last)
+void fano(my*P, int first, int last)
 {
 	if (first == last) return;
 	if (first + 1 == last)
@@ -46,11 +46,15 @@ void fano(my*P , int first, int last)
 		if (sumchast > 0.5)
 		{
 			numsum = i;
+			if (numsum == 0)
+			{
+				numsum = 1;
+			}
 			break;
 		}
 	}
 
-	
+
 	for (int i = first; i < numsum; i++)
 	{
 		AddChar(P[i].code, '0');
@@ -59,48 +63,40 @@ void fano(my*P , int first, int last)
 	{
 		AddChar(P[i].code, '1');
 	}
-
-	/*fano(P, first, numsum - 1);
-	fano(P, numsum, last);*/
-
+	
+	fano(P, numsum, last);
+	fano(P, first, numsum-1);
 }
 
 int vs[256] = { 0 };
 int N;
-char str[20] = "brbn";
+char str[20] = "aaaaaaaaabc";
 
 int main()
 {
 	int i = 0;
-
 	while (str[i] != 0)
 	{
 		vs[str[i]]++;
 		i++;
 	}
-
 	for (int i = 0; i < 256; i++)
 		if (vs[i] != 0)
 		{
 			N++;
-		}																													
-
+		}
 	my * new_struct = new my[N];
-
 	int j = 0;
 	float i_new = i;
-
 	for (int i = 0; i < 256; i++)
 	{
 		if (vs[i] != 0)
 		{
 			new_struct[j].ch = i;
 			new_struct[j].kol = vs[i];
-		 
 			j++;
 		}
 	}
-
 	for (int i = 0; i < N; i++)
 	{
 		(new_struct + i)->code[0] = '\0';
@@ -112,10 +108,7 @@ int main()
 		{
 			if (new_struct[j].kol < new_struct[j + 1].kol)
 			{
-				my tmp = new_struct[j];
-				new_struct[j] = new_struct[j + 1];
-				new_struct[j + 1];
-			/*	int tmp = new_struct[j].kol;
+				int tmp = new_struct[j].kol;
 				char tmp_ch = new_struct[j].ch;
 				float tmp_chast = new_struct[j].chast;
 				new_struct[j].kol = new_struct[j + 1].kol;
@@ -124,7 +117,7 @@ int main()
 				new_struct[j + 1].kol = tmp;
 				new_struct[j + 1].ch = tmp_ch;
 				new_struct[j + 1].chast = tmp_chast;
-				flag = 0;*/
+				flag = 0;
 			}
 		}
 		if (flag)
@@ -132,53 +125,46 @@ int main()
 			break;
 		}
 	}
-
 	for (int i = 0; i < N; i++)
 	{
 		printf("%c %d %s\n", (new_struct + i)->ch, (new_struct + i)->kol, (new_struct + i)->code);
 	}
-
 	fano(new_struct, 0, N - 1);
-
 	for (int i = 0; i < N; i++)
 	{
-		printf("%c  %s\n", (new_struct + i) -> ch, (new_struct + i) -> code);
+		printf("%c  %s\n", (new_struct + i)->ch, (new_struct + i)->code);
 	}
-
 	return 0;
 }
-
-
-
 /*
 void arr_sort(int* ptr, int arrLen)
 {
-	int i, j;
-	for (i = 0; i < arrLen - 1; i++)
-	{
-		for (j = 0; j < arrLen - i - 1; j++)
-		{
-			if (ptr[j] > ptr[j + 1])
-			{
-				int temp = ptr[j];
-				ptr[j] = ptr[j + 1];
-				ptr[j + 1] = temp;
-			}
-		}
-	}
-	for (i = 0; i < arrLen; i++)
-	{
-		printf("%d ", ptr[i]);
-	}
-	printf("\n");
+int i, j;
+for (i = 0; i < arrLen - 1; i++)
+{
+for (j = 0; j < arrLen - i - 1; j++)
+{
+if (ptr[j] > ptr[j + 1])
+{
+int temp = ptr[j];
+ptr[j] = ptr[j + 1];
+ptr[j + 1] = temp;
+}
+}
+}
+for (i = 0; i < arrLen; i++)
+{
+printf("%d ", ptr[i]);
+}
+printf("\n");
 }
 
 
 void main()
 {
-	int arr[] = { 1231241, 1232, 33, 5, 213, 532 };
-	int len = sizeof(arr)/sizeof(len);
+int arr[] = { 1231241, 1232, 33, 5, 213, 532 };
+int len = sizeof(arr)/sizeof(len);
 
-	arr_sort(arr, len);
+arr_sort(arr, len);
 }
 */
